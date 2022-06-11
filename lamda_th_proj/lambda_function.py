d8b5b037bc325fcd54fd8f7954d0750e01824407
Lhercam <105813574+Lhercam@users.noreply.github.com>
2022-06-11 21:22:12 +0200

    added functions build_request & extract_cost_data
    build_request takes an account number string and returns a dict that is parametrized for the get-cost-and-usage function in the API
    extract_cost_data takes a dict returned by get-cost-and-usage and extracts a pandas DataFrame containing only the Service names and the costs

--- lamda_th_proj/lambda_function.py
+++ lamda_th_proj/lambda_function.py
@@ -1,6 +1,10 @@
 import boto3
 import os
 import uuid
+import datetime
+import pandas as pd
+import json
+
 
 #implement getcostandusage api to get json
 
@@ -22,3 +26,27 @@ def writeToDynamo(event, context):
     )
 
     return recordId
+
+
+
+def build_request(account_nr):
+    res = {'TimePeriod': {'Start': '', 'End': ''},
+           'Granularity': 'MONTHLY',
+           'Filter': {'Dimensions': {'Key': 'LINKED_ACCOUNT', 'Values': [account_nr]}},
+           'Metrics': ['BlendedCost'],
+           'GroupBy': [{'Type': 'DIMENSION', 'Key': 'SERVICE'}]}
+    d = datetime.date.today()
+    end = datetime.date(d.year, d.month, 1)
+    start = datetime.date(d.year, d.month - 1, 1)
+    res['TimePeriod']['End'] = str(end)
+    res['TimePeriod']['Start'] = str(start)
+    return res
+
+
+def extract_cost_data(data):
+    temp = data['ResultsByTime'][0]['Groups']
+    df = pd.DataFrame(temp)
+    for x in range(df.size // 2):
+        df.loc[x, 'Metrics'] = df.loc[x, 'Metrics']['BlendedCost']['Amount']
+        df.loc[x, 'Keys'] = df.loc[x, 'Keys'][0]
+    return df
