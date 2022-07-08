d50ee20ead2a346542ebed2a3262b9b6d78ac551
Mat Hirs <101565669+mathirs@users.noreply.github.com>
2022-07-08 19:03:46 +0200

    Added functionality to attach cost data document

--- ses_email/lambda_function.py
+++ ses_email/lambda_function.py
@@ -1,26 +1,86 @@
 import json
 import boto3
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from io import StringIO, BytesIO
+from docx import Document
+from docx.shared import Inches, Pt
 from botocore.exceptions import ClientError
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 
 def lambda_handler(event, context):
-    
-    
-    emailData = event['Records'][0]["messageAttributes"]
+    client = boto3.resource("dynamodb")
+    table = client.Table("CostData_THB")
+    costdata = table.scan()['Items']
+    # get cost data in dataframe
+    data = costdata[0]
+    costs = data['data']
+
+    # verify clean plot
+    plt.clf()
+
+    bargraph = BytesIO()
+    New_Colors = ['#0089BA', '#00A7C5', '#00C2BB', '#48DAA2', '#A3ED84', '#F9F871', '#5FBEF2']
+    plt.bar(range(len(costs)), list(costs.values()), align='center', color=New_Colors)
+    plt.title('AWS Cost Data', fontsize=14)
+    plt.xlabel('Services', fontsize=14)
+    plt.ylabel('Cost', fontsize=14)
+    plt.xticks(range(len(costs)), list(costs.keys()), rotation=45)
+    plt.legend(loc="upper left")
+
+    plt.savefig(bargraph, format='png', bbox_inches='tight')
+    bargraph.seek(0)
+
+    # piechart 
+    # Data to plot
+    # verify clean plot
+
+    plt.clf()
+    piechart = BytesIO()
+    labels = []
+    sizes = []
+
+    for x, y in costs.items():
+        labels.append(x)
+        sizes.append(y)
+
+    # Plot
+    plt.pie(sizes, labels=labels)
+
+    plt.axis('equal')
+    plt.savefig(piechart, format='png', bbox_inches='tight')
+    piechart.seek(0)
+
+    document = Document()
+    document.add_heading('Cost Data Visualization', 0)
+    p = document.add_paragraph('Report')
+    document.add_picture(bargraph, height=Inches(3.641732), width=Inches(5.1889764))
+    document.add_picture(piechart, height=Inches(3.641732), width=Inches(5.1889764))
+    doc = BytesIO()
+    document.save(doc)
+    doc.seek(0)
+
+    send_email(doc)
+
+def send_email(doc):
+    # emailData = event['Records'][0]["messageAttributes"]
     # print(emailData['fromName'])
-    
+
     # Replace sender@example.com with your "From" address.
     # This address must be verified with Amazon SES.
-    SENDER = emailData['fromName']['stringValue'] + " <marketing@example.com>"
-
+    SENDER = "Cost Data Messenger" + " <projthbingen@gmail.com>"
 
     # Replace recipient@example.com with a "To" address. If your account 
     # is still in the sandbox, this address must be verified.
-    RECIPIENT = emailData['toEmail']['stringValue']
-    
-    print (emailData)
-    
-    if 'toCCEmail' in emailData:
+    RECIPIENT = "jonas.trojahn@th-bingen.de"
+
+    # print(emailData)
+
+    """if 'toCCEmail' in emailData:
       CC_RECIPIENT = emailData['toCCEmail']['stringValue']
       DESTINATION={
                         'ToAddresses': [
@@ -33,70 +93,94 @@ def lambda_handler(event, context):
                             'marketingcc@example.com',
                         ]
                     }
-      
-    else:
-      DESTINATION = {
-                        'ToAddresses': [
-                            RECIPIENT,
-                        ],
-                        'BccAddresses': [
-                            'marketingcc@example.com',
-                        ]
-                    }
-    
-    
+
+    else:"""
+    DESTINATION = {
+        'ToAddresses': [
+            RECIPIENT,
+        ],
+        'BccAddresses': [
+            'projthbingen@gmail.com',
+        ]
+    }
+
     # Specify a configuration set. If you do not want to use a configuration
     # set, comment the following variable, and the 
     # ConfigurationSetName=CONFIGURATION_SET argument below.
-    #CONFIGURATION_SET = "ConfigSet"
-    
+    # CONFIGURATION_SET = "ConfigSet"
+
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-    AWS_REGION = "ap-southeast-2"
-    
+    AWS_REGION = "us-east-1"
+
     # The subject line for the email.
-    SUBJECT = emailData['subject']['stringValue']
-    
+    SUBJECT = "AWS Cost Report"
+
+    # The full path to the file that will be attached to the email.
+    #ATTACHMENT = ""
+
     # The email body for recipients with non-HTML email clients.
-    # BODY_TEXT = ("Amazon SES Test (Python)\r\n"
-    #              "This email was sent with Amazon SES using the "
-    #              "AWS SDK for Python (Boto)."
-    #             )
-                
+    #BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+    #             "This email was sent with Amazon SES using the "
+    #             "AWS SDK for Python (Boto)."
+    #            )
+
     # The HTML body of the email.
-    BODY_HTML = emailData['body']['stringValue']          
-    
+    BODY_HTML = "Guten Tag,\nIm Anhang finden Sie den Kostenbericht für diesen Monat"
+    # emailData['body']['stringValue']          
+
     # The character encoding for the email.
     CHARSET = "UTF-8"
-    
+
     # Create a new SES resource and specify a region.
-    client = boto3.client('ses',region_name=AWS_REGION)
+    client = boto3.client('ses', region_name=AWS_REGION)
+
+    # Create a multipart/mixed parent container.
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = SUBJECT 
+    msg['From'] = SENDER 
+    msg['To'] = RECIPIENT
+
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+
+    # Encode the text and HTML content and set the character encoding. This step is
+    # necessary if you're sending a message with characters outside the ASCII range.
+    textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+    #htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET) 
+
+    # Add the text and HTML parts to the child container.
+    msg_body.attach(textpart)
+    #msg_body.attach(htmlpart)
+
+    # Define the attachment part and encode it using MIMEApplication.
+    att = MIMEApplication(doc.read())
     
+    # Add a header to tell the email client to treat this part as an attachment,
+    # and to give the attachment a name.
+    att.add_header('Content-Disposition','attachment',filename='test.docx')
+
+    # Attach the multipart/alternative child container to the multipart/mixed
+    # parent container.
+    msg.attach(msg_body)
+
+    # Add the attachment to the parent container.
+    msg.attach(att)
+    #print(msg)
+
     # Try to send the email.
     try:
-        #Provide the contents of the email.
-        response = client.send_email(
-            Destination = DESTINATION,
-            Message={
-                'Body': {
-                    'Html': {
-                        'Charset': CHARSET,
-                        'Data': BODY_HTML,
-                    },
-                    # 'Text': {
-                    #     'Charset': CHARSET,
-                    #     'Data': BODY_TEXT,
-                    # },
-                },
-                'Subject': {
-                    'Charset': CHARSET,
-                    'Data': SUBJECT,
-                },
-            },
+        # Provide the contents of the email.
+        response = client.send_raw_email(
             Source=SENDER,
-            # If you are not using a configuration set, comment or delete the
-            # following line
-            #ConfigurationSetName=CONFIGURATION_SET,
+            Destinations=[
+                RECIPIENT
+            ],
+            RawMessage={
+                        'Data': msg.as_string(),
+                    },
         )
+        
     # Display an error if something goes wrong.	
     except ClientError as e:
         print(e.response['Error']['Message'])
@@ -104,9 +188,3 @@ def lambda_handler(event, context):
         print("Email sent! Message ID:")
         print(response['MessageId'])
         print(RECIPIENT)
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response)
-    }
-    
