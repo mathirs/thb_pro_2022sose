1982ea38d581af84b7c5d56f27639d3a369364ad
Lhercam <105813574+Lhercam@users.noreply.github.com>
2022-07-08 18:41:52 +0200

    Added multiaccount support

--- lamda_th_proj/lambda_function.py
+++ lamda_th_proj/lambda_function.py
@@ -6,41 +6,53 @@ import pandas as pd
 import json
 
 
-
-
 def lambda_handler(event, context):
-    #for multiple accounts touch up
-    aws_account_id = context.invoked_function_arn.split(":")[4]
-    request = build_request(aws_account_id)
-    client = boto3.client('ce')
-    costData = client.get_cost_and_usage(
-        TimePeriod=request['TimePeriod'], Granularity=request['Granularity'], Filter=request['Filter'], Metrics=request['Metrics'], GroupBy=request['GroupBy'])
-    writeToDynamo(costData, aws_account_id)
+    sts = boto3.client('sts')
+    dynamo = boto3.resource('dynamodb')
+    accounts = dynamo.Table("Accounts_THB")
+    data = accounts.scan()['Items']
+    this_account_id = context.invoked_function_arn.split(":")[4]
+    for line in data:
+        aws_account_id = line['AccountNr']
+        credentials = None
+        if aws_account_id != this_account_id:
+            credentials = sts.assume_role(RoleArn=line['ARN'], RoleSessionName='CostExplorerAccess')
+        request = build_request(aws_account_id)
+        if credentials:
+            cost_explorer = boto3.client(
+                'ce',
+                aws_access_key_id=credentials["Credentials"]["AccessKeyId"],
+                aws_secret_access_key=credentials["Credentials"]["SecretAccessKey"],
+                aws_session_token=credentials["Credentials"]["SessionToken"])
+        else:
+            cost_explorer = boto3.client('ce')
+        cost_data = cost_explorer.get_cost_and_usage(
+            TimePeriod=request['TimePeriod'], Granularity=request['Granularity'], Filter=request['Filter'], Metrics=request['Metrics'], GroupBy=request['GroupBy'])
+        write_to_dynamo(cost_data, aws_account_id)
     return {
         'statusCode': 200,
         'body': json.dumps('Dynamo !')
     }
 
 
-def writeToDynamo(event, accountnr):
+def write_to_dynamo(event, accountnr):
 
     recordId = str(uuid.uuid4())
     timestamp = event["ResultsByTime"][0]["TimePeriod"]["End"]
     data = extract_cost_data(event)
 
-    #Creating new record in DynamoDB table
+    # Creating new record in DynamoDB table
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table("Cost_Data_thbt1")
+    table = dynamodb.Table("CostData_THB")
     table.put_item(
         Item={
-            'Costdata' : accountnr, #placeholder replace with account number
+            'AccountNr' : accountnr,
             'Date' : timestamp,
             'data' : data
         }
     )
 
     return recordId
-
 
 
 def build_request(account_nr):
@@ -67,3 +79,4 @@ def extract_cost_data(data):
         else:
             res[df.loc[x, 'Keys'][0]] = df.loc[x, 'Metrics']['BlendedCost']['Amount']
     return res
+
